0b32e13f3e4df190096dd8e45b8b08bc917b66e7
nadeemAshfaq <nadeemashfaq141117@gmail.com>
2024-06-19 12:19:14 +0500

    mongo added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/advocite', {
+mongoose.connect('mongodb://nadeemashfaqit:12345@ac-lrv53ag-shard-00-00.tvhcw8m.mongodb.net:27017,ac-lrv53ag-shard-00-01.tvhcw8m.mongodb.net:27017,ac-lrv53ag-shard-00-02.tvhcw8m.mongodb.net:27017/advocite?ssl=true&replicaSet=atlas-9eskos-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
